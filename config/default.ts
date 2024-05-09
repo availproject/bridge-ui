503c107fedd29f8256a70005fb93ddfe464ceaac
Abheek Tripathy <abheek.tripathy@gmail.com>
2024-05-09 15:12:47 +0400

    update envs for deployment

--- config/default.ts
+++ config/default.ts
@@ -24,8 +24,8 @@ export const appConfig: AppConfig = Object.freeze({
     networks: {
         ethereum: sepolia,
     },
-    bridgeApiBaseUrl: 'http://0.0.0.0:8080',
-    bridgeIndexerBaseUrl: 'http://167.71.41.169:3000',
+    bridgeApiBaseUrl: process.env.NEXT_PUBLIC_BRIDGE_API_URL || 'http://0.0.0.0:8080',
+    bridgeIndexerBaseUrl: process.env.NEXT_PUBLIC_BRIDGE_INDEXER_URL_2 ||'http://167.71.41.169:3000',
     bridgeIndexerPollingInterval: 30, // in seconds
     contracts: {
         ethereum: {
