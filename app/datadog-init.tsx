e25616bcd58efdb4599b09dd3ac6036452267eea
Abheek Tripathy <abheek.tripathy@gmail.com>
2024-08-20 21:50:11 +0530

    feedback impl

--- app/datadog-init.tsx
+++ app/datadog-init.tsx
@@ -2,9 +2,6 @@
 
 import { datadogRum } from "@datadog/browser-rum";
 
-
-console.log("Datadog Init", process.env.NEXT_PUBLIC_DATADOG_RUM_APPLICATION_ID, process.env.NEXT_PUBLIC_DATADOG_RUM_CLIENT_TOKEN);
-
 datadogRum.init({
   applicationId: process.env.NEXT_PUBLIC_DATADOG_RUM_APPLICATION_ID || "",
   clientToken: process.env.NEXT_PUBLIC_DATADOG_RUM_CLIENT_TOKEN || "",
@@ -12,14 +9,21 @@ datadogRum.init({
   service: "avail-bridge-ui",
   env: process.env.NEXT_PUBLIC_ENVIRONMENT || "staging",
   version: "1.0.0",
-  sessionSampleRate: 100,
-  sessionReplaySampleRate: 20,
-  trackUserInteractions: true,
-  trackResources: true,
-  trackLongTasks: true,
+  sessionSampleRate: Number(process.env.NEXT_PUBLIC_SESSION_SAMPLE_RATE) || 100,
+  sessionReplaySampleRate: Number(process.env.NEXT_PUBLIC_SESSION_REPLAY_SAMPLE_RATE) || 20,
+  trackUserInteractions: process.env.NEXT_PUBLIC_TRACK_USER_INTERACTIONS === 'true',
+  trackResources: process.env.NEXT_PUBLIC_TRACK_RESOURCES === 'true',
+  trackLongTasks: process.env.NEXT_PUBLIC_TRACK_LONG_TASKS === 'true',
   defaultPrivacyLevel: "mask-user-input",
 });
 
 export default function DatadogInit() {
   return null;
 }
+
+
+// sessionSampleRate: 100,
+// sessionReplaySampleRate: 20,
+// trackUserInteractions: true,
+// trackResources: true,
+// trackLongTasks: true,
