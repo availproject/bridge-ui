f4df8e0f1996b575a4114672dd0cfaab5b139b6b
Jeremy <jeremias@availproject.org>
2024-08-23 10:08:33 -0300

    feat: Adds DD log lib

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,18 +1,22 @@
+import { datadogLogs } from '@datadog/browser-logs'
+
+datadogLogs.init({
+  clientToken: process.env.NEXT_PUBLIC_DATADOG_RUM_CLIENT_TOKEN || "",
+  site: process.env.NEXT_PUBLIC_DD_HOST || "datadoghq.com",
+  forwardErrorsToLogs: true,
+  sessionSampleRate: 100,
+})
+
 export class Logger {
     static log(message: string) {
-        console.log(message);
+        datadogLogs.logger.info(message);
     }
 
     static debug(message: string) {
-        if (process.env.NODE_ENV === "production") {
-            return
-        }
-
-        console.debug(message);
+        datadogLogs.logger.debug(message);
     }
 
     static error(message: string) {
-        // error logging/monitoring service
-        console.error(message);
+        datadogLogs.logger.error(message);
     }
 }
