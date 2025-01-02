e374f816c0cd9e90d951b4f5bfd3129a77f4999d
0xAbhk <90976669+abheektripathy@users.noreply.github.com>
2025-01-02 06:08:35 +0530

    Delete hooks/Metamask directory

--- hooks/Metamask/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './MetamaskContext';
-export * from './useInvokeSnap';
-export * from './useMetaMask';
-export * from './useRequest';
-export * from './useRequestSnap';
-
-
-export const defaultSnapOrigin = process.env.SNAP_ORIGIN ?? `npm:@avail-project/avail-snap`;
