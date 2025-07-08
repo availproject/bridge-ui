81346ef143346e3a6f3d7bbcd17ac642936ec028
Abheek Tripathy <abheek.tripathy@gmail.com>
2025-07-08 15:26:51 +0530

    update copy

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -10,8 +10,8 @@ export default function NavBar() {
       >
         <InfoIcon className="w-10 h-10 md:h-3 md:w-3 mr-2 text-white text-opacity-60" />{" "}
         <span className="text-white text-xs text-opacity-60 max-md:text-left">
-          Heads up! The avail metamask snap might not work as intended at the
-          moment - we&apos;re working on fixing this.
+          Heads up! The Avail Snap might not work as intended at the moment -
+          we&apos;re working on fixing this.
         </span>
       </div>
       <div className="pt-4 space-x-4 w-screen flex flex-row items-center justify-center">
