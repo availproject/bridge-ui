a76e87202ed7645e36029af7db6f2d241a87ef95
Jakub Cech <3016965+jakubcech@users.noreply.github.com>
2025-12-16 13:51:03 +0100

    cve fix + transpile using fast compile

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    transpilePackages: ['@polkadot/types-codec'],
+    transpilePackages: ['@tanstack/query-core'],
     reactStrictMode: true,
     webpack: config => {
       config.externals.push('pino-pretty', 'lokijs', 'encoding');
